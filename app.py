fdc85d2e5b746e44811a8412fdcd4d4a99605fda
unknown <michael.chian9@gmail.com>
2020-12-23 20:54:06 -0800

    final commit

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import xml.etree.ElementTree as ET
 import urllib.request
+from flask import Flask, render_template, session, request, send_file, Response, redirect, url_for, flash
+from flask_session import Session
 
-#app = Flask(__name__)
+app = Flask(__name__)
 
 namespaces = {'place': 'http://www.sec.gov/edgar/document/thirteenf/informationtable',
 'other_thing': 'http://www.w3.org/2001/XMLSchema-instance'}
@@ -70,20 +72,27 @@ def parseWeb(url):
     file = open("data.txt", "a")
     file.write("Company                            Holdings\n")
     for stock in stocks:
-        for n in range(35 - len(stock["name"])):
-            spaces += " "
-        file.write(stock["name"] + spaces + "{:,}".format(stock["amount"]) + "\n")
+        file.write(stock["name"] + "\t" + "{:,}".format(stock["amount"]) + "\n")
         spaces = ""
     file.close()
     stocks.clear()
+
+found1 = []
+found2 = []
+# 0 = not found, 1 = found
 
 def compare():
     bought = []
     sold = []
     same = []
+
+    copy1 = allHoldings[0].copy()
+    copy2 = allHoldings[1].copy()
+
     for stock in allHoldings[0]:
         for s in allHoldings[1]:
             if stock["name"] == s["name"]:
+                found1.append(1)
                 change = stock["amount"] - s["amount"]
                 changes = {
                     "name": stock["name"],
@@ -97,11 +106,11 @@ def compare():
                     bought.append(changes)
                 else:
                     same.append(changes)
-                allHoldings[0].remove(stock)
-                allHoldings[1].remove(s)
+                copy1.remove(stock)
+                copy2.remove(s)
                 break
     #print("checking for new ones")
-    for stock in allHoldings[0]:
+    for stock in copy1:
         #print(stock)
         changes = {
             "name": stock["name"],
@@ -110,7 +119,7 @@ def compare():
             "amount": stock["amount"]
         }
         sold.append(changes)
-    for stock in allHoldings[1]:
+    for stock in copy2:
         changes = {
             "name": stock["name"],
             "change": stock["amount"],
@@ -125,9 +134,9 @@ def compare():
     for stock in bought:
         for n in range(35 - len(stock["name"])):
             spaces += " "
-        file.write(stock["name"] + spaces + "{:,}".format(stock["change"]))
+        file.write(stock["name"] + "\t" + "{:,}".format(stock["change"]))
         if stock["isAll"] == True:
-            file.write(" (New company)")
+            file.write("\t(New company)")
         file.write("\n")
         spaces = ""
 
@@ -136,9 +145,9 @@ def compare():
     for stock in sold:
         for n in range(35 - len(stock["name"])):
             spaces += " "
-        file.write(stock["name"] + spaces + "{:,}".format(stock["change"]))
+        file.write(stock["name"] + "\t" + "{:,}".format(stock["change"]))
         if stock["isAll"] == True:
-            file.write(" (All stocks of that company)")
+            file.write("\t(All stocks of that company)")
         file.write("\n")
         spaces = ""
     
@@ -147,33 +156,34 @@ def compare():
     for stock in same:
         for n in range(35 - len(stock["name"])):
             spaces += " "
-        file.write(stock["name"] + spaces + "{:,}".format(stock["amount"]))
+        file.write(stock["name"] + "\t" + "{:,}".format(stock["amount"]))
         file.write("\n")
         spaces = ""
     file.close()
 
-
-def main():
+@app.route("/", methods=["POST", "GET"])
+def primary():
     #parseXML()
+    if request.method == "POST":
 
-    print("Paste link to first xml file: ")
-    url1 = input()
-    #url1 = "https://www.sec.gov/Archives/edgar/data/1067983/000095012320009058/960.xml"
-    file = open("data.txt", "w")
-    file.write("The first holding:\n")
-    file.close()
-    parseWeb(url1)
-    print("Paste link to second xml file:")
-    url2 = input()
-    #url2 = "https://www.sec.gov/Archives/edgar/data/1067983/000095012320012127/0000950123-20-012127-1653.xml"
-    file = open("data.txt", "a")
-    file.write("The second holding:\n")
-    file.close()
-    parseWeb(url2)
-    compare()
-
-#if __name__ == "__main__":
-#    app.run(debug=True)
+        url1 = request.form["link1"]
+        url2 = request.form["link2"]
+        #url1 = "https://www.sec.gov/Archives/edgar/data/1067983/000095012320009058/960.xml"
+        file = open("data.txt", "w")
+        file.write("The first holding:\n")
+        file.close()
+        parseWeb(url1)
+        #url2 = "https://www.sec.gov/Archives/edgar/data/1067983/000095012320012127/0000950123-20-012127-1653.xml"
+        file = open("data.txt", "a")
+        file.write("The second holding:\n")
+        file.close()
+        parseWeb(url2)
+        compare()
+        with open('data.txt', 'r') as file:
+            fulltext = file.read()
+        return render_template("index.html", table=fulltext)
+    else:
+        return render_template("index.html", table="")
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
